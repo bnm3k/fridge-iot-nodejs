978f54c988fed63990a3972af51df0feee79c3ef
nagamocha3000 <nagamocha@gmail.com>
2019-10-30 12:43:21 +0300

    add tls to client

--- client/fridge_client.js
+++ client/fridge_client.js
@@ -1,6 +1,7 @@
 "use strict";
-const net = require("net");
-const reconnect = require("reconnect-net");
+const tls = require("tls");
+const path = require("path");
+const fs = require("fs");
 const multiplex = require("multiplex");
 const chalk = require("chalk");
 const { processFridgeEventsStream } = require("./utils/fridgeEvents");
@@ -8,11 +9,28 @@ const setupRpc = require("./utils/setupRpc");
 const runCli = require("./utils/cli");
 
 const { host, port, cmdOpts } = runCli(process.argv);
-onServerConn(net.createConnection(port), cmdOpts);
+const certsPath = path.resolve(path.dirname(__filename), "../certs");
+const connOpts = {
+    host,
+    port,
+    ca: [fs.readFileSync(path.join(certsPath, "certificate.pem"))],
+    rejectUnauthorized: true
+};
+const server = tls.connect(connOpts);
+server.once("connect", () => {
+    console.log(chalk.gray(`connected to server on port: ${port}`));
+    onServerConn(server, cmdOpts);
+});
+server.on("close", () => console.log(chalk.gray("Server closed connection")));
+server.on("error", err => {
+    if (err.code === "ECONNREFUSED")
+        console.error(
+            chalk.red(`Cannot connect to server ${err.address}:${err.port}`)
+        );
+    else console.error(err);
+});
 
 function onServerConn(fridgeServer, cmdOpts) {
-    console.log(chalk.gray("connected"));
-
     const plex = multiplex((stream, id) => {
         if (id === "temp" || id === "door") {
             processFridgeEventsStream(stream);
